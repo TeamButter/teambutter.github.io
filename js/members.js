a701fc8995c1bed71de305ab6231d2035d7cfad0
Avinaba Dalal <d97.avinaba@gmail.com>
2016-06-01 22:21:03 +0530

    web: Basic bringup

--- js/members.js
+++ js/members.js
@@ -1,32 +1,39 @@
-/*var xmlhttp = new XMLHttpRequest();
-var url = "data/json/member_list.json";
+var xmlhttp = new XMLHttpRequest();
+var url = "data/json/members.json";
 
 xmlhttp.onreadystatechange=function() {
     if (xmlhttp.readyState == 4 && xmlhttp.status == "200") {
-        myJsonParser(xmlhttp.responseText);
-    }
+			var arr = JSON.parse(xmlhttp.responseText);
+			var i;
+			var out = null;
+			for(i = 0; i < arr.length; i++) {
+				var n_url = "https://api.github.com/users/" + arr[i].name;
+				console.log(n_url);
+				var request = new XMLHttpRequest();
+				request.onreadystatechange=function() {
+					if (request.readyState == 4 && request.status == "200") {
+						console.log("got response");
+						var mJSONobj = JSON.parse(request.responseText);
+						if(out == null)
+							out = "<li><img src=\""+mJSONobj.avatar_url+"\">" + mJSONobj.login + "</li>";
+						else
+							out += "<li><img src=\""+mJSONobj.avatar_url+"\">" + mJSONobj.login + "</li>";
+					}
+				}
+				request.open("GET",n_url,false);
+				request.send();
+			}
+			document.getElementById("member_block").innerHTML = out;
+		}
 }
 xmlhttp.open("GET", url, true);
-xmlhttp.send();*/
+xmlhttp.send();
 
 function myJsonParser(response) {
-    //var arr = JSON.parse(response);
-    var i;
-    var out;
-    for(i = 0; i < 1; i++) {
-        var request = new XMLHttpRequest();
-	// Set the event handler
-	request.onload = console.log(this.responseText);;
-	// Initialize the request
-	request.open('get', 'https://api.github.com/users/funchal', true)
-	// Fire away!
-	request.send()
-    }
-    document.getElementById("member_block").innerHTML = out;
+    
     
 }
 
-function getAvatar(response,out,name) {
-        	var mJSONobj = JSON.parse(response);
-        	out += "<li><img src=\"" + mJSONobj.avatar_url + ">"+mJSONobj.login+"</li>"; 
+function getAvatar(response,out) {
+        	
 }
